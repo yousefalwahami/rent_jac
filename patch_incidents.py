7ecdab514f8642e7c3b53c682b9f0ddf3c7a56f3
JadNizam <jadnizam@hotmail.com>
2026-04-05 03:11:41 -0400

    chore: remove all build and patch python scripts

--- patch_incidents.py
+++
@@ -1,9 +0,0 @@
-with open("incidents.cl.jac", "r") as f:
-    text = f.read()
-
-if "cl import from .workflow { AiFlowDetailView }" not in text:
-    text = "cl import from .workflow { AiFlowDetailView }\n" + text
-
-with open("incidents.cl.jac", "w") as f:
-    f.write(text)
-
